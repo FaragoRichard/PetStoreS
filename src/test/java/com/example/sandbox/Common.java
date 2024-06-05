5e44ec456493dfe908bbf73b791fc6a74f320c20
Faragó Richárd <farago.richard12@gmail.com>
2024-06-05 21:19:54 +0200

    Extending Common with additional methods

--- src/test/java/com/example/sandbox/Common.java
+++ src/test/java/com/example/sandbox/Common.java
@@ -1,7 +1,9 @@
 package com.example.sandbox;
 
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
+import java.io.File;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
@@ -24,6 +26,37 @@ public class Common extends Endpoints {
                 .extract().response();
 
     }
+    public Response getUrlWithPath(String endpoint, Map<String, String> pathParams){
+        return given()
+                .relaxedHTTPSValidation()
+                .pathParams(pathParams)
+                .and()
+                .log().everything()
+                .when()
+                .get(baseUrl+endpoint)
+                .then()
+                .log()
+                .all()
+                .extract().response();
+
+    }
+
+    public Response getUrlWithPath(String endpoint, Map<String, String> queryParam, Map<String, String> pathParams){
+        return given()
+                .relaxedHTTPSValidation()
+                .params(queryParam)
+                .pathParams(pathParams)
+                .and()
+                .log().all()
+                .when()
+                .get(baseUrl+endpoint)
+                .then()
+                .log()
+                .all()
+                .extract().response();
+
+    }
+
     public Response getUrl(String endpoint, Map<String, String> queryParam ){
 
 
@@ -66,6 +99,61 @@ public class Common extends Endpoints {
                 .relaxedHTTPSValidation()
                 .contentType("application/json; charset=UTF-8")
                 .body(body)
+                .and()
+                .log().everything()
+                .when()
+                .post(baseUrl+endpoint)
+                .then()
+                .log()
+                .all()
+                .extract().response();
+
+    }
+
+    public Response putUrl(String endpoint, String body, Map<String, String> pathParams ){
+        return given()
+                .relaxedHTTPSValidation()
+                .pathParams(pathParams)
+                .contentType("application/json")
+                .body(body)
+                .and()
+                .log().everything()
+                .when()
+                .put(baseUrl+endpoint)
+                .then()
+                .log()
+                .all()
+                .extract().response();
+    }
+
+    public Response postUrl(String endpoint, Map<String, String> formParams, Map<String, String> pathParams ){
+
+
+        return given()
+                .relaxedHTTPSValidation()
+                .formParams(formParams)
+                .pathParams(pathParams)
+                .contentType("application/x-www-form-urlencoded")
+                .and()
+                .log().everything()
+                .when()
+                .post(baseUrl+endpoint)
+                .then()
+                .log()
+                .all()
+                .extract().response();
+
+    }
+
+    public Response postFile(String endpoint, String filePath, Map<String, String> pathParams ){
+
+
+        return given()
+                .relaxedHTTPSValidation()
+                .multiPart("file", new File(filePath))
+                .pathParams(pathParams)
+                .contentType(ContentType.MULTIPART)
+                .accept(ContentType.JSON)
                 .and()
                 .log().everything()
                 .when()
