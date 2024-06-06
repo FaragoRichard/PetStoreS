432ccef25b5446c027f621c5a4407b7441ada72d
Faragó Richárd <farago.richard12@gmail.com>
2024-06-06 11:01:24 +0200

    Common test setup for Failed user tests

--- src/test/java/com/example/sandbox/endpointTest/UserTest_Fail.java
+++ src/test/java/com/example/sandbox/endpointTest/UserTest_Fail.java
@@ -3,6 +3,7 @@ package com.example.sandbox.endpointTest;
 import com.example.sandbox.Common;
 import io.restassured.response.Response;
 import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.util.Map;
@@ -12,36 +13,32 @@ import static com.example.sandbox.util.constans.Tags.SMOKE;
 
 public class UserTest_Fail extends Common {
 
+    @BeforeTest
+    public void userSetup(){
+        Map<String, String> pathParams = new TreeMap<>();
+        pathParams.put("username", "ThereIsAbsolutelyNoWayThisUserExists");
+
+        Response response = getUrlWithPath(user, pathParams);
+        Assert.assertEquals(response.getStatusCode(),404,"Invalid response code");
+    }
     @Test(enabled = true, groups = {SMOKE}, description = "Get user by name", priority = 2)
     public void GetUserByName_Fail_NonExistentUsername(){
         Map<String, String> pathParams = new TreeMap<>();
         pathParams.put("username", "ThereIsAbsolutelyNoWayThisUserExists");
 
         Response response = getUrlWithPath(user, pathParams);
-        Assert.assertEquals(response.getStatusCode(),404,"Invalid response code");
-
-        Map<String, String> pathParams2 = new TreeMap<>();
-        pathParams2.put("username", "ThereIsAbsolutelyNoWayThisUserExists");
-
-        Response response2 = getUrlWithPath(user, pathParams2);
-        Assert.assertNotEquals(response2.getStatusCode(),200,"Invalid response code");
-        Assert.assertEquals(response2.getStatusCode(),404, "Invalid response code");
+        Assert.assertNotEquals(response.getStatusCode(),200,"Invalid response code");
+        Assert.assertEquals(response.getStatusCode(),404, "Invalid response code");
     }
 
     @Test(enabled = true, groups = {SMOKE}, description = "Login user", priority = 2)
     public void LoginUser_Fail_InvalidData(){
-        Map<String, String> pathParams = new TreeMap<>();
-        pathParams.put("username", "ThereIsAbsolutelyNoWayThisUserExists");
-
-        Response response = getUrlWithPath(user, pathParams);
-        Assert.assertEquals(response.getStatusCode(),404,"Invalid response code");
-
         Map<String, String> queryParams = new TreeMap<>();
         queryParams.put("username", "ThereIsAbsolutelyNoWayThisUserExists");
         queryParams.put("password", "VerySecurePassword");
 
-        Response response2 = getUrl(login, queryParams);
-        Assert.assertNotEquals(response2.getStatusCode(),200,"Invalid response code");
-        Assert.assertEquals(response2.getStatusCode(),400,"Invalid response code");
+        Response response = getUrl(login, queryParams);
+        Assert.assertNotEquals(response.getStatusCode(),200,"Invalid response code");
+        Assert.assertEquals(response.getStatusCode(),400,"Invalid response code");
     }
 }
